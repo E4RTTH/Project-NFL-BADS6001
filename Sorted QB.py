f987038eef5362dd91ebf935ff293f14ef1eb491
E4RTTH <kavin.sin@stu.nida.ac.th>
2020-11-15 14:30:10 +0700

    Update Sorted QB.py

--- Sorted QB.py
+++ Sorted QB.py
@@ -5,21 +5,24 @@ qb = combine["Pos"].isin(["QB"])
 position_QB = combine[qb]
 # position_QB.to_csv(r'D:\NIDA\Intro BADS\Project BADS6001\Project-NFL--BADS6001-\NameQB.csv',index=False)
 
-# Code for scraping colledge data QB
-n = []
+# # Code for scraping colledge data QB
+# n = [i.lower().replace('.', '').replace(' ', '-') for i in position_QB.Player]
 
-for i in position_QB.Player:
-  n.append(i.lower().replace('.', '').replace(' ', '-'))
-x = {}
-college = {}
+# # for i in position_QB.Player:
+# #   n.append(i.lower().replace('.', '').replace(' ', '-'))
 
-cl = 'https://www.sports-reference.com/cfb/players/{}-1.html'
+# x = {}
+# college = {}
 
-for c in range(20):
-  cl.replace('1',str(c))
-  for i in n:
-    try:
-      x['{}'.format(i)] = pd.read_html(cl.format(i))
-      college['{}'.format(i)] = x['{}'.format(i)][0]
-    except:
-      pass
+# cl = 'https://www.sports-reference.com/cfb/players/{}-1.html'
+
+# for i in n:
+#   for c in range(1,20):
+#     cl.replace('1',str(c))
+#     try:
+#       x['{}'.format(i)] = pd.read_html(cl.format(i))
+#       college['{}'.format(i)] = x['{}'.format(i)][0]
+#     except:
+#       break
+
+# print(college)
