cfbf02c4d5d0d199317ceeef4ff5d5a55322460c
E4RTTH <kavin.sin@stu.nida.ac.th>
2020-11-15 13:03:38 +0700

    revise code

--- Sorted QB.py
+++ Sorted QB.py
@@ -4,3 +4,21 @@ combine = pd.read_csv(r'D:\NIDA\Intro BADS\Project BADS6001\Project-NFL--BADS600
 qb = combine["Pos"].isin(["QB"])
 position_QB = combine[qb]
 position_QB.to_csv(r'D:\NIDA\Intro BADS\Project BADS6001\Project-NFL--BADS6001-\NameQB.csv',index=False)
+
+n = []
+
+for i in position_QB.Player:
+  n.append(i.lower().replace('.', '').replace(' ', '-'))
+x = {}
+college = {}
+
+cl = 'https://www.sports-reference.com/cfb/players/{}-1.html'
+
+for c in range(20):
+  cl.replace('1',str(c))
+  for i in n:
+    try:
+      x['{}'.format(i)] = pd.read_html(cl.format(i))
+      college['{}'.format(i)] = x['{}'.format(i)][0]
+    except:
+      pass
