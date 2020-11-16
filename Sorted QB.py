99d570308b4422ebcf3b355c8d81e07bfa3852cc
E4RTTH <kavin.sin@stu.nida.ac.th>
2020-11-16 22:11:25 +0700

    Clean data QB

--- Sorted QB.py
+++ Sorted QB.py
@@ -1,26 +1,29 @@
 #sort QB Data from Combine 2010 - 2019
 import pandas as pd
-combine = pd.read_csv(r'D:\EARTH\NIDA\NFL Project\Project-NFL-BADS6001\combine 2010 - 2019.csv',encoding= 'UTF-8')
+combine = pd.read_csv(r'D:\NIDA\Intro BADS\Project BADS6001\Project-NFL--BADS6001-\combine 2010 - 2019.csv',encoding= 'UTF-8')
 qb = combine["Pos"].isin(["QB"])
 position_QB = combine[qb]
-# position_QB.to_csv(r'D:\NIDA\Intro BADS\Project BADS6001\Project-NFL--BADS6001-\NameQB.csv',index=False)
 
 # Code for scraping colledge data QB
-n = [i.lower().replace('.', '').replace(' ', '-') for i in position_QB.Player]
+n = [i.lower().replace('.', '').replace("'",'').replace(' ', '-') for i in position_QB.Player] #clean name for web scraping
 x = {}
 college = {}
-cl = 'https://www.sports-reference.com/cfb/players/{}-1.html'
+cl = 'https://www.sports-reference.com/cfb/players/{}-{}.html'
 
 for i in n:
-  for c in range(1,2):
-    cl.replace('1',str(c))
+  for c in range(1,3):
     try:
-      x['{}'.format(i)] = pd.read_html(cl.format(i))
-      college['{}'.format(i)] = x['{}'.format(i)][0]
+      x['{}-{}'.format(i,c)] = pd.read_html(cl.format(i,c))
+      college['{}-{}'.format(i,c)] = x['{}-{}'.format(i,c)][0]
     except:
       break
 
-# print(college['nick-fitzgerald'])
-# print(type(college['nick-fitzgerald']))
-# print(college['nick-fitzgerald'].shape)
-# print(college['nick-fitzgerald'].columns)
+keys = list(college.keys())
+for a in keys:
+  df = college[a]
+  position = df['Unnamed: 4_level_0']
+  if position['Pos'].iloc[1] != 'QB':
+    college.pop(a)
+
+print(college)
+print(len(college))
