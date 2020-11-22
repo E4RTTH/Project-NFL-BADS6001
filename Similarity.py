9a665ff22d99dd5558d1652b50deb041686bb239
suphanat <natkhung0905@gmail.com>
2020-11-23 00:35:17 +0700

    เพิ่มรายละเอียด

--- Similarity.py
+++ Similarity.py
@@ -1,15 +1,17 @@
 #load data
 import pandas as pd
 import numpy as np
-qb_stats = pd.read_csv(r'D:\NIDA\Intro BADS\Project BADS6001\Project-NFL-BADS6001\Train_data.csv',encoding= 'UTF-8')
+qb_stats = pd.read_csv(r'D:\NIDA\Intro BADS\Project BADS6001\Project-NFL-BADS6001\Train_data1.csv',encoding= 'UTF-8')
+img = pd.read_csv(r'D:\NIDA\Intro BADS\Project BADS6001\Project-NFL-BADS6001\img.csv',encoding= 'UTF-8')
 
 #Similarity
 from scipy.spatial.distance import euclidean,pdist,squareform
 user_input = {'userinput from website'}
 qb_drafted = qb_stats[qb_stats['y']==1].fillna(0)
-euclidean_max = []
+similarity = {}
 
 for i in range(0,len(qb_drafted.index)):
-    euclidean_max.append(euclidean(user_input,i))
+    similarity[qb_drafted.iloc[i][0]] = euclidean(user_input,qb_drafted.iloc[i][1:])
+                                                  
+url_img = img[min(similarity)] 
 
-max(euclidean_max)
